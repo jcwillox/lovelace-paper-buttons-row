a71f76d33d0494ba7a5597239952c193cd7bde42
Josh Willox <joshwillox@gmail.com>
2021-12-14 21:14:49 +1100

    Use more robust method for inserting buttons into entity rows

--- src/generic-entity-row.js
+++ src/generic-entity-row.js
@@ -6,29 +6,48 @@ const ELEMENT = "hui-generic-entity-row";
 
 customElements.whenDefined(ELEMENT).then(() => {
   const Element = customElements.get(ELEMENT);
+  if (!Element) return;
   const oFirstUpdated = Element.prototype.firstUpdated;
 
   Element.prototype.firstUpdated = function (changedProperties) {
     oFirstUpdated.call(this, changedProperties);
 
     if (this.config.extend_paper_buttons_row) {
-      let paperButtons = createEntityRow({
+      const paperButtons = createEntityRow({
         type: "custom:paper-buttons-row",
         ...this.config.extend_paper_buttons_row
       });
 
       provideHass(paperButtons);
 
+      let el = this.shadowRoot.querySelector("slot");
+      if (el.parentElement) {
+        if (el.parentElement.classList.contains("text-content")) {
+          el = el.parentElement;
+        } else {
+          console.error("unexpected parent node found");
+        }
+      }
+
       if (this.config.extend_paper_buttons_row.position === "right") {
-        this.shadowRoot.appendChild(paperButtons);
+        insertAfter(paperButtons, el);
       } else {
-        this.shadowRoot.insertBefore(
-          paperButtons,
-          this.shadowRoot.lastElementChild
-        );
+        insertBefore(paperButtons, el);
       }
     }
   };
 
   fireEvent("ll-rebuild", {});
 });
+
+function insertBefore(node, element) {
+  element.parentNode.insertBefore(node, element);
+}
+
+function insertAfter(node, element) {
+  if (element.nextElementSibling) {
+    insertBefore(node, element.nextElementSibling);
+  } else {
+    element.parentNode.appendChild(node);
+  }
+}
